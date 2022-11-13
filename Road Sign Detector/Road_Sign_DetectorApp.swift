ad8fd2364a259a78d11734e9866233e898b933a6
Jake <Jake3231@users.noreply.github.com>
2022-11-13 01:40:10 -0600

    Initial Commit

---
+++ Road Sign Detector/Road_Sign_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Road_Sign_DetectorApp.swift
+//  Road Sign Detector
+//
+//  Created by Jake Spann on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Road_Sign_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
